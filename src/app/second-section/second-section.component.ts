b00f2768b5375f808b98242d7740a084cde3d03f
andy-codebox <andy.sim@57codebox.com>
2024-10-01 09:48:28 +0800

    sup

---
+++ src/app/second-section/second-section.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-second-section',
+  standalone: true,
+  imports: [],
+  templateUrl: './second-section.component.html',
+  styleUrl: './second-section.component.scss'
+})
+export class SecondSectionComponent {
+
+}
