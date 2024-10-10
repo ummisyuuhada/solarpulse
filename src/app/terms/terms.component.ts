63c4bd37f64913122dc302f763da9be080804d11
farahA04 <fawahayuwi@gmail.com>
2024-10-10 13:33:40 +0800

    merge

--- src/app/terms/terms.component.ts
+++ src/app/terms/terms.component.ts
@@ -1,10 +1,24 @@
+// import { Component } from '@angular/core';
+// import { RouterModule } from '@angular/router';
+
+// @Component({
+//   selector: 'app-terms',
+//   standalone: true,
+//   imports: [RouterModule],
+//   templateUrl: './terms.component.html',
+//   styleUrl: './terms.component.scss'
+// })
+// export class TermsComponent {
+
+// }
+
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+
 
 @Component({
   selector: 'app-terms',
   standalone: true,
-  imports: [RouterModule],
+  imports: [],
   templateUrl: './terms.component.html',
   styleUrl: './terms.component.scss'
 })
