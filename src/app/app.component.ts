bda6ef510dc4dbb1d314ec071089d6d6f7071bfd
farahA04 <fawahayuwi@gmail.com>
2024-10-10 10:32:37 +0800

    v1.7.2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
+import { filter } from 'rxjs/operators'; // test
+import { NgIf } from '@angular/common'; // test
 import { HeaderComponent } from "./header/header.component";
 import { HomeComponent } from "./home/home.component";
 import { SecondSectionComponent } from "./second-section/second-section.component";
@@ -8,14 +10,31 @@ import { ThirdSectionComponent } from "./third-section/third-section.component";
 import { FourthSectionComponent } from "./fourth-section/fourth-section.component";
 import { ContactUsComponent } from "./contact-us/contact-us.component";
 import { TermsComponent } from './terms/terms.component';
+import { PrivacyPolicyComponent } from "./privacy-policy/privacy-policy.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, HomeComponent, SecondSectionComponent, AboutUsComponent, ThirdSectionComponent, FourthSectionComponent, ContactUsComponent, TermsComponent],
+  imports: [RouterOutlet, NgIf, HeaderComponent, HomeComponent, SecondSectionComponent, AboutUsComponent, ThirdSectionComponent, FourthSectionComponent, ContactUsComponent, TermsComponent, PrivacyPolicyComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'solarpulse';
+  // test
+  showGeneralContent = true;
+
+  constructor(private router: Router) {
+    // Listen to router events and update whether to show general content
+    this.router.events.pipe(
+      filter(event => event.constructor.name === 'NavigationEnd')
+    ).subscribe(() => {
+      const currentRoute = this.router.url;
+      this.showGeneralContent = !(currentRoute === '/privacy-policy' || currentRoute === '/terms');
+
+      setTimeout(() => {
+        window.scrollTo(0, 0); // Scroll to the top of the page
+      }, 100);
+    });
+  }
 }
